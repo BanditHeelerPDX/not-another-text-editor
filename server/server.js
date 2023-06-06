8948c6db7bc6b90c1fd99e70952960186d6ba0e9
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-06 14:00:31 -0700

    I do not know what I am doing at all

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const staticPath = path.resolve(__dirname, '../client/dist');
 
-app.use(express.static('../client/dist'));
+app.use(express.static(staticPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
