e1b3f72c6b66f087024a77f3b81eb045c6d3fff6
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-05 23:38:59 -0700

    client webpack configured

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const 
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,12 +19,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'NATE',
+        short_name: 'NATE',
+        description: 'NATE',
+        background_color: '#c5e1a5',
+        theme_color: '#f4e8ad',
+        icons: [
+          {
+            src: path.resolve(__dirname, 'client/src/favicon.ico'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            type: 'image/x-icon',
+          },
+          {
+            src: path.resolve(__dirname, 'client/src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            purpose: 'any maskable',
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test:/\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            }
+          }
+        }
       ],
     },
   };
