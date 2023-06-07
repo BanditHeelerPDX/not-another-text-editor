03b760c59c61c319c9fe5c3c6354fbfa3a331b6d
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-07 10:40:54 -0700

    webpack fixed with assistance from Alex Marriott

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,68 +1,75 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "../dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin'
+        template: "./index.html",
+        title: "NATE ",
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
       }),
       new WebpackPwaManifest({
-        name: 'NATE',
-        short_name: 'NATE',
-        description: 'NATE',
-        background_color: '#c5e1a5',
-        theme_color: '#f4e8ad',
+        name: "NATE",
+        short_name: "NATE",
+        description: "just another text editor",
+        background_color: "#c5e1a5",
+        theme_color: "#f4e8ad",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve(__dirname, 'favicon.ico'),
+            src: path.resolve("/favicon.ico"),
             sizes: [96, 128, 192, 256, 384, 512],
-            type: 'image/x-icon',
+            type: "image/x-icon",
           },
           {
-            src: path.resolve(__dirname, 'src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            purpose: 'any maskable',
-          }
-        ]
-      })
+            purpose: "any maskable",
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
         {
-          test:/\.js$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-            }
-          }
-        }
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
