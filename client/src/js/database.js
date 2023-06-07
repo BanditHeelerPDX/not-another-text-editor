9855662e82e998dc8d73652d3294a3780e7edc02
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-06-07 11:15:34 -0700

    uniformity

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,15 +17,15 @@ export const putDb = async (content) => {
     const db = await openDB("jate", 1);
     const tx = db.transaction("jate", "readwrite");
     const store = tx.objectStore("jate");
-    const request = store.add({ jate: content });
+    const request = store.put({ id: 1, value: content });
     const result = await request;
     console.log("Data put", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
